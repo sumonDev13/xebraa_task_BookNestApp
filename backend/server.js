b8538f4e786eb2afcc7810c319950dfca574df0a
sumonDev <sumonmondal1530765@gmail.com>
2024-12-25 11:29:02 +0600

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
